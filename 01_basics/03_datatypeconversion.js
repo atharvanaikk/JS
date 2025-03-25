f8caee600f1aa37d877c9233634fbad9f053b0f2
atharvanaikk <atharvagnaik@gmail.com>
2025-03-25 18:43:56 +0000

    conversion

---
+++ 01_basics/03_datatypeconversion.js
@@ -0,0 +1,8 @@
+let score = "atharva"
+// console.log(typeof score)
+// console.log(typeof (score));
+
+let changed_score = Number (score)
+console.log(typeof changed_score);
+// always check the output datatype after conversion 
+

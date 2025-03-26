ba9495ccf62914bc8d6c1d253a3accb460276a7a
atharvanaikk <atharvagnaik@gmail.com>
2025-03-26 05:13:29 +0000

    operators

--- 01_basics/03_datatypeconversion.js
+++ 01_basics/03_datatypeconversion.js
@@ -1,8 +1,29 @@
-let score = "atharva"
+/*let score = "atharva"
 // console.log(typeof score)
 // console.log(typeof (score));
 
 let changed_score = Number (score)
 console.log(typeof changed_score);
 // always check the output datatype after conversion 
+*/
 
+/* Operations*/
+
+// let str1 = atharva
+// let str2 =  naik
+
+// console.log(str1 + str2);
+
+// console.log("1"+2);
+// console.log(2+2+2+"5");
+// console.log(+"");
+
+// let num1, num2, num3
+// num1=num2=num3=4
+
+let gamecounter = 69
+
+++gamecounter //precidense operator prefix
+
+gamecounter++ // precidense operator postfix
+console.log(gamecounter);

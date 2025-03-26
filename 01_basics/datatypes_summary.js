4dfc5987995fdcfea3f80d100b85838a8c770d22
atharvanaikk <atharvagnaik@gmail.com>
2025-03-26 08:46:01 +0000

    datatypessummary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,16 @@
+// Primitive => 7 Types
+// String, Number, Boolean, null, undefined, Symbol, Bigint
+
+// Refrence (Non-Primitive)
+// Array, Objects, Functions type of datatype is always function 
+// and of function is object function
+
+
+const heros = ["Hero1", "Hero2"] //array
+let myObj = { name : "hero1", age : 22} //obj
+
+const MyFunction = function (){
+    console.log("Hello World");
+    
+}
+// JS is dynamic language 

2f64bc4f3db8acc5788cf42a9ff552bc95584334
atharvanaikk <atharvagnaik@gmail.com>
2025-03-26 05:23:37 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,7 @@
+// console.log("2">1); here 2 is converted into number 
+console.log(null>=0);//true
+console.log(null>0);//false
+console.log(undefined>0);// for undefined everything is false
+// avoid such type of comparison btw null and undefined
+
+
